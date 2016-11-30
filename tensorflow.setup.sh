8b0e7e76cf52f7720549c0b98a62bcbbb55b6b03
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2016-11-30 20:49:34 +0900

    Create tensorflow.setup.sh
    first version.

---
+++ tensorflow.setup.sh
@@ -0,0 +1,10 @@
+# refer https://github.com/samjabrahams/tensorflow-on-raspberry-pi
+# For Python 2.7
+wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/raw/master/bin/tensorflow-0.10.0-cp27-none-linux_armv7l.whl
+sudo pip install tensorflow-0.10.0-cp27-none-linux_armv7l.whl
+rm -rf tensorflow-0.10.0-cp27-none-linux_armv7l.whl
+
+# For Python 3.3+
+wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/raw/master/bin/tensorflow-0.10.0-py3-none-any.whl
+sudo pip install tensorflow-0.10.0-py3-none-any.whl
+rm -rf tensorflow-0.10.0-py3-none-any.whl
