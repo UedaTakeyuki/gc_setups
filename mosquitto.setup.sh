2e48edd855a4b87ebedd2b2012061b4a9e78af51
Ueda Takeyuki <ueda@latelierdueda.com>
2016-12-01 10:35:19 +0900

    complete to fix. now can run well.

--- mosquitto.setup.sh
+++ mosquitto.setup.sh
@@ -1,5 +1,7 @@
 # refer https://www.justinribeiro.com/chronicle/2014/10/22/mosquitto-libwebsockets-google-compute-engine-setup/
 
+c_path=`pwd`
+
 # libwebsokets-dev is unmatch with mosquitto.
 #sudo apt-get install uuid-dev libwebsockets-dev libssl-dev libc-ares-dev
 sudo apt-get install uuid-dev libssl-dev libc-ares-dev
@@ -15,13 +17,17 @@ sudo ln -s /usr/local/lib64/libwebsockets.so.4.0.0 /usr/lib/libwebsockets.so.4.0
 sudo ln -s /usr/local/lib64/libwebsockets.so /usr/lib/libwebsockets.so
 sudo ln -s /usr/local/lib64/libwebsockets.a /usr/lib/libwebsockets.a
 
+cd $c_path
 #mosquitto 1.4.4 (with websocket, latest version seems more confusing.)
 wget https://mosquitto.org/files/source/mosquitto-1.4.4.tar.gz
 tar -xvzf mosquitto-1.4.4.tar.gz
 cd mosquitto-1.4.4
-sudo sed -i 's|^WITH_WEBSOCKETS:=no|^WITH_WEBSOCKETS:=yes|' config.mk
+sudo sed -i 's|^WITH_WEBSOCKETS:=no|WITH_WEBSOCKETS:=yes|' config.mk
 make binary
 sudo make install
-sh -c "echo listener 1883 >> /etc/mosquitto/mosquitto.conf"
-sh -c "echo listener 10001 127.0.0.1 >> /etc/mosquitto/mosquitto.conf"
-sh -c "echo protocol websockets >> /etc/mosquitto/mosquitto.conf"
+sudo sh -c "echo listener 1883 >> /etc/mosquitto/mosquitto.conf"
+sudo sh -c "echo listener 9000 127.0.0.1 >> /etc/mosquitto/mosquitto.conf"
+sudo sh -c "echo protocol websockets >> /etc/mosquitto/mosquitto.conf"
+
+cd $c_path
+sudo rm -rf libwebsockets-1.3-chrome37-firefox30 v1.3-chrome37-firefox30.tar.gz mosquitto-1.4.4 mosquitto-1.4.4.tar.gz
