9a5ea9d5a9d6da016f30fa03a4d5bfdac85b36a7
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2017-06-07 17:17:01 +0900

    salt-minion

---
+++ salt-minion.setup.sh
@@ -0,0 +1,9 @@
+# https://docs.saltstack.com/en/develop/topics/installation/debian.html
+sudo sh -c "echo 'deb http://archive.raspbian.org/raspbian/ stretch main' >> /etc/apt/sources.list"
+sudo sh -c "echo 'http://httpredir.debian.org/debian jessie-backports main contrib' >> /etc/apt/sources.list"
+sudo sh -c "echo 'APT::Default-Release "jessie";' > /etc/apt/apt.conf.d/10apt"
+
+sudo apt-get update
+
+sudo apt-get install python-systemd/jessie-backports python-zmq python-tornado/stretch salt-common/stretch
+sudo apt-get insall salt-minion/stretch
