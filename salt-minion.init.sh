09e0eb484901ef8e3777c72a3ff78f2988e2736e
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2017-06-08 19:30:27 +0900

    remove /etc/salt

--- salt-minion.init.sh
+++ salt-minion.init.sh
@@ -4,8 +4,15 @@ if [ $# -ne 1 ]; then
 fi
 master=$1
 serial=`python -m piserialnumber`
+
+# rpurge previous salt
 sudo apt-get purge salt-common salt-minion
+sudo rm -rf /etc/salt
+
+# reinstall salt-common & salt-minion
 sudo apt-get install salt-common/stretch salt-minion/stretch
+
+# settings
 sudo sed -i "s/#master: salt/master: ${master}/" /etc/salt/minion
 sudo sed -i "s/#id:/id: gc16_${serial}/" /etc/salt/minion
 sudo systemctl restart salt-minion
