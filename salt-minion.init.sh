a6ac9064ea8fac3ba23731cb853d1adf61988a10
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2017-08-25 17:35:15 +0900

    fix: can init with running minion

--- salt-minion.init.sh
+++ salt-minion.init.sh
@@ -16,7 +16,7 @@ sudo openssl genrsa 2048 > /etc/salt/pki/minion/minion.pem
 sudo openssl rsa -in /etc/salt/pki/minion/minion.pem -pubout -out /etc/salt/pki/minion/minion.pub
 
 # settings
-sudo sed -i "s/#master: salt/master: ${master}/" /etc/salt/minion
+sudo sed -i "s/master: [a-zA-Z\.0-9]*/master: ${master}/" /etc/salt/minion
 #sudo sed -i "s/#id:/id: gc16_${serial}/" /etc/salt/minion
-sudo sed -i "s/#id:/id: gc_${uuid}/" /etc/salt/minion
+sudo sed -i "s/id: [a-zA-Z0-9_\-]*/id: gc_${uuid}/" /etc/salt/minion
 sudo systemctl restart salt-minion
