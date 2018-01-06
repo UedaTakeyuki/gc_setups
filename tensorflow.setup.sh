1e13c598ef26febd62ede2fe6f22bc9b40eb4fc3
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2018-01-06 16:42:14 +0900

    v1.1.0

--- tensorflow.setup.sh
+++ tensorflow.setup.sh
@@ -1,10 +1,22 @@
 # refer https://github.com/samjabrahams/tensorflow-on-raspberry-pi
 # For Python 2.7
-wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/raw/master/bin/tensorflow-0.10.0-cp27-none-linux_armv7l.whl
-sudo pip install tensorflow-0.10.0-cp27-none-linux_armv7l.whl
-rm -rf tensorflow-0.10.0-cp27-none-linux_armv7l.whl
+sudo apt-get install python-pip python-dev
+#wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/raw/master/bin/tensorflow-0.10.0-cp27-none-linux_armv7l.whl
+#sudo pip install tensorflow-0.10.0-cp27-none-linux_armv7l.whl
+#rm -rf tensorflow-0.10.0-cp27-none-linux_armv7l.whl
+wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/releases/download/v1.1.0/tensorflow-1.1.0-cp27-none-linux_armv7l.whl
+sudo pip install tensorflow-1.1.0-cp27-none-linux_armv7l.whl
+rm -rf tensorflow-1.1.0-cp27-none-linux_armv7l.whl
+sudo pip uninstall mock
+sudo pip install mock
 
 # For Python 3.3+
-wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/raw/master/bin/tensorflow-0.10.0-py3-none-any.whl
-sudo pip3 install tensorflow-0.10.0-py3-none-any.whl
-rm -rf tensorflow-0.10.0-py3-none-any.whl
+sudo apt-get install python3-pip python3-dev
+#wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/raw/master/bin/tensorflow-0.10.0-py3-none-any.whl
+#sudo pip3 install tensorflow-0.10.0-py3-none-any.whl
+#rm -rf tensorflow-0.10.0-py3-none-any.whl
+wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/releases/download/v1.1.0/tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl
+sudo pip3 install tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl
+rm -rf tensorflow-1.1.0-cp34-cp34m-linux_armv7l.whl
+sudo pip3 uninstall mock
+sudo pip3 install mock
