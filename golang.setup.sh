b217b0176c91a813e4b9edd8962a3d696f6445fc
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2018-06-30 21:10:20 +0900

    export

--- golang.setup.sh
+++ golang.setup.sh
@@ -1,3 +1,6 @@
 wget https://golang.org/dl/go1.10.3.linux-armv6l.tar.gz
 sudo tar -C /usr/local -xzf go1.10.3.linux-armv6l.tar.gz
 rm go1.10.3.linux-armv6l.tar.gz
+
+## add export to .bashrc
+#  export PATH=$PATH:/usr/local/go/bin
