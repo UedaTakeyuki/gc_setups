ea6c8b8a313f202ef5245e1a864a593ad2e69c37
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2018-10-26 08:32:09 +0000

    install pip3

--- aiortc.setup.sh
+++ aiortc.setup.sh
@@ -1,2 +1,2 @@
-apt install libavdevice-dev libavfilter-dev libopus-dev libvpx-dev pkg-config libsrtp2-1
-pip3 install aiortc
+sudo apt install libavdevice-dev libavfilter-dev libopus-dev libvpx-dev pkg-config libsrtp2-1 python3-pip
+sudo pip3 install aiortc
