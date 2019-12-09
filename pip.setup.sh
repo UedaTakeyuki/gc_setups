5f25556afbfb64e5346ff4f4eb378ecf88838db1
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2019-12-09 10:29:37 +0000

    pip

---
+++ pip.setup.sh
@@ -0,0 +1,4 @@
+# Confirmed with Raspberry Pi reference 
+# - Raspbian 2019-09-26 ver (Linux raspberrypi 4.19.75+ #1270 Tue Sep 24 18:38:54 BST 2019 armv6l GNU/Linux)
+
+sudo apt install python-pip python3-pip
