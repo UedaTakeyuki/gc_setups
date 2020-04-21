6d9d0cdc7673faa1423cdd1d4047a1425526dd48
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2020-04-21 22:01:20 +0900

    add libffi-dev

--- aiortc.setup.sh
+++ aiortc.setup.sh
@@ -1,3 +1,4 @@
 sudo apt install libavdevice-dev libavfilter-dev libopus-dev libvpx-dev pkg-config libsrtp2-1 python3-pip
 sudo apt install libsrtp2-dev
+sudo apt install libffi-dev
 sudo pip3 install aiortc
