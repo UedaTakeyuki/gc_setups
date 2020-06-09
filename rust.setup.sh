07e41c5436f71474d3be1d6db2f35c68eed38e95
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2020-06-09 10:42:28 +0900

    add editting of .profile

--- rust.setup.sh
+++ rust.setup.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+if [[ ! "$PATH" =~ ${HOME}/.cargo/bin ]]; then
+  cat << EOF >>~/.profile
+# set PATH so it includes cargo/bin bin if it exists
+if [ -d "${HOME}/.cargo/bin" ] ; then
+    PATH="${HOME}/.cargo/bin:$PATH"
+fi
+EOF
+
+  echo do source ~/.profile to set cargo/bin in path.
+
+fi
+
