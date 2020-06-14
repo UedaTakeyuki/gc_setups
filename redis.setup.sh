a65a828c6c79f6331420ba70569dac0c5cdfb8a6
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2020-06-14 11:21:11 +0900

    redis latest

---
+++ redis.setup.sh
@@ -0,0 +1,19 @@
+# sudo apt-get -y install redis
+
+# https://redis.io/topics/quickstart
+
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+sudo make install
+sudo mkdir /etc/redis
+sudo mkdir /var/redis
+sudo cp utils/redis_init_script /etc/init.d/redis_6379
+sudo cp redis.conf /etc/redis/6379.conf
+sudo mkdir /var/redis/6379
+sudo update-rc.d redis_6379 defaults
+sudo /etc/init.d/redis_6379 start
+cd ..
+rm -rf redis-stable.tar.gz
+rm -rf redis-stable
