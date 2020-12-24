9a1a7a21d8db268015cc7fc1d9aca146d848f333
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2020-12-24 14:20:41 +0900

    usbmount for buster

--- usbmount.setup.sh
+++ usbmount.setup.sh
@@ -1,2 +1,8 @@
 # https://www.raspberrypi.org/forums/viewtopic.php?t=205016
 sudo apt-get install usbmount
+
+# https://raspberrypi.stackexchange.com/questions/100312/raspberry-4-usbmount-not-working
+sudo mkdir -p /etc/systemd/system/systemd-udevd.service.d
+
+sudo sh -c "echo \[Service\] > /etc/systemd/system/systemd-udevd.service.d/00-my-custom-mountflags.conf"
+sudo sh -c "echo PrivateMounts=no >> /etc/systemd/system/systemd-udevd.service.d/00-my-custom-mountflags.conf"
