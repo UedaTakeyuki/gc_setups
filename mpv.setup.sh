6a8cd350e2e0898d2e40aff7ed3a36b1b92ec8f4
Dr. Takeyuki Ueda <gde00107@nifty.com>
2021-11-01 16:32:00 +0900

    mpv

---
+++ mpv.setup.sh
@@ -0,0 +1,7 @@
+# mpv
+sudo apt-get install mpv
+
+# youtube-dl
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+youtube-dl --version
