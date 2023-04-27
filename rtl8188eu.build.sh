056740a970291ba98a10eaf9ee5af4eb34dac249
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2023-04-27 01:43:37 +0100

    dkms build of rtl8188eu

---
+++ rtl8188eu.build.sh
@@ -0,0 +1,15 @@
+# https://forums.raspberrypi.com/viewtopic.php?t=331841
+# https://forums.raspberrypi.com/viewtopic.php?t=332364
+# https://github.com/ivanovborislav/rtl8188eu
+# https://qiita.com/nuruposan/items/84a8d6bc1911b32a915e
+
+# https://github.com/ivanovborislav/rtl8188eu
+#sudo apt-get install bc build-essential
+#sudo sh -c "git clone https://github.com/ivanovborislav/rtl8188eu.git /usr/src/rtl8188eu-$(uname -r)"
+sudo apt-get install dkms
+cd /usr/src/rtl8188eu-$(uname -r)
+sudo sh -c "sed -i 's@CONFIG_PLATFORM_I386_PC = y@CONFIG_PLATFORM_I386_PC = n@' Makefile"
+sudo sh -c "sed -i 's@CONFIG_PLATFORM_RPI_ARM = n@CONFIG_PLATFORM_RPI_ARM = y@' Makefile"
+sudo dkms add -m rtl8188eu -v $(uname -r)
+sudo dkms autoinstall
+dkms status
