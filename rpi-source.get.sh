fb169e1aa302bda2fb16e4717cfff32d8b032876
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2023-05-15 14:19:04 +0100

    add rpi-source

---
+++ rpi-source.get.sh
@@ -0,0 +1,3 @@
+sudo apt install -y python2 bc git flex bison libssl-dev libncurses5-dev
+sudo wget https://raw.githubusercontent.com/RPi-Distro/rpi-source/master/rpi-source -O /usr/local/bin/rpi-source && sudo chmod +x /usr/local/bin/rpi-source && /usr/local/bin/rpi-source -q --tag-update
+rpi-source
