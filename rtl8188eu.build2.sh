3c26440f2440e99a954d10b2f113f14c0ea8348f
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2023-05-16 07:01:23 +0100

    rtl8188eu.build2.sh

---
+++ rtl8188eu.build2.sh
@@ -0,0 +1,16 @@
+# install requires
+sudo apt-get install bc build-essential dkms
+
+# get rpi-source
+#./rpi-source.get.sh
+
+# Prevent loading old driver r8188eu
+sudo sh -c "echo blacklist r8188eu > /etc/modprobe.d/50-8188eu.conf"
+
+# clone rtl8188eus
+git clone https://github.com/aircrack-ng/rtl8188eus.git
+
+# build
+cd rtl8188eus
+sudo ./dkms-install.sh
+
