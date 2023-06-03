9dcb2fd3570d0e6316df2ad7b6fd3a32423f71c4
Dr. Takeyuki Ueda <ueda@latelierdueda.com>
2023-06-03 12:55:31 +0900

    Update dist-upgrade.jessie.stretch.sh

--- dist-upgrade/dist-upgrade.jessie.stretch.sh
+++ dist-upgrade/dist-upgrade.jessie.stretch.sh
@@ -2,3 +2,7 @@ sudo sed -i 's/jessie/stretch/g' /etc/apt/sources.list
 sudo sed -i 's/jessie/stretch/g' /etc/apt/sources.list.d/raspi.list
 sudo apt-get update
 sudo apt-get -y dist-upgrade
+# install new Chinese and Japanese fonts
+sudo apt-get install -y fonts-droid-fallback
+# purge PulseAudio
+sudo apt-get -y purge "pulseaudio*"
