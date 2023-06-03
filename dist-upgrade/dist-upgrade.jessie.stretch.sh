6fb6417893a31e505c007d7bd944bcfd5cea2f8e
Dr. Takeyuki Ueda <ueda@latelierdueda.com>
2023-06-03 12:56:26 +0900

    Update dist-upgrade.jessie.stretch.sh

--- dist-upgrade/dist-upgrade.jessie.stretch.sh
+++ dist-upgrade/dist-upgrade.jessie.stretch.sh
@@ -1,3 +1,4 @@
+# https://djangocas.dev/blog/upgrade-existing-jessie-to-buster-without-re-image
 sudo sed -i 's/jessie/stretch/g' /etc/apt/sources.list
 sudo sed -i 's/jessie/stretch/g' /etc/apt/sources.list.d/raspi.list
 sudo apt-get update
