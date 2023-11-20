fae74f6845dcf573831ff4b9b4c8b5933241fd71
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2023-11-20 13:19:06 +0900

    avoide expandation of variable inside the here string

--- golang/golang.setup.sh
+++ golang/golang.setup.sh
@@ -12,8 +12,10 @@ wget https://golang.org/dl/${file}
 sudo tar -C /usr/local -xzf ${file}
 rm ${file}
 
+# set path to .profile
+# don't expand variable. https://linux.just4fun.biz/?%E9%80%86%E5%BC%95%E3%81%8D%E3%82%B7%E3%82%A7%E3%83%AB%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88/%E3%83%92%E3%82%A2%E3%83%89%E3%82%AD%E3%83%A5%E3%83%A1%E3%83%B3%E3%83%88%E5%86%85%E3%81%AE%E5%A4%89%E6%95%B0%E3%82%92%E5%B1%95%E9%96%8B%E3%81%97%E3%81%AA%E3%81%84%E6%96%B9%E6%B3%95
 if [[ ! "$PATH" =~ /usr/local/go/bin ]]; then
-  cat << EOF >>~/.profile
+  cat << 'EOF' >>~/.profile
 
 # set PATH so it includes go bin if it exists
 if [ -d "/usr/local/go/bin" ] ; then
