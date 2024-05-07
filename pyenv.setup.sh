8e68792becac5e05d7d339e99da6127f8d415f2c
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2024-05-07 20:09:19 +0900

    add environement setup also to .bashrc for interactive shells

--- pyenv.setup.sh
+++ pyenv.setup.sh
@@ -1,6 +1,7 @@
+# install pyenv
 curl https://pyenv.run | bash
 
-# set path to .profile
+# set pyenv to .profile for login shells
 cat << 'EOF' >>~/.profile
 
 # Set up your shell environment for Pyenv
@@ -8,3 +9,12 @@ export PYENV_ROOT="$HOME/.pyenv"
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 EOF
+
+# set pyenv to .bashrc for interactive shells
+cat << 'EOF2' >>~/.bashrc 
+
+# Set up your shell environment for Pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+EOF2
