0ac56a69db74088d9877dbb31d5c57c88b9457f9
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2024-06-21 18:42:33 +0900

    add json-glib

--- golang/golang.setup.sh
+++ golang/golang.setup.sh
@@ -22,10 +22,22 @@ if [ -d "/usr/local/go/bin" ] ; then
     PATH="/usr/local/go/bin:$PATH"
 fi
 EOF
-
-  echo do source ~/.profile to set go in path.
-
 fi
+
+# set PATH so it includes the the path to where a binary file compiled by the command "go install" is installed. 
+cat << 'EOF2' >>~/.profile
+# set PATH so it includes the the path to where a binary file compiled by the command "go install" is installed. 
+if [ -n "`go env GOBIN`" ]; then
+    PATH=`go env GOBIN`:$PATH
+fi
+if [ -n "`go env GOPATH`" ]; then
+    PATH=`go env GOPATH`/bin:$PATH
+elif [ -n "$HOME" ]; then
+    PATH=$HOME/go/bin
+fi
+EOF2
+
+echo do source ~/.profile to set go in path.
 
 ## add export to /etc/profile (for system-wide use) or ~/.profiley
 #export PATH=$PATH:/usr/local/go/bin
