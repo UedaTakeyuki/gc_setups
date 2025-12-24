c2135db1f473ce5ce412698f18185909aca194ea
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2025-12-24 21:40:49 +0900

    updated

--- RPiSpecificConf/moveToLegacyBuster.sh
+++ RPiSpecificConf/moveToLegacyBuster.sh
@@ -1,9 +1,12 @@
 # move default repository from http://raspbian.raspberrypi.org/raspbian/ to http://legacy.raspbian.org/raspbian/
 awk '{
   if ($0 !~ /^#/){
-    buff = $0;
-    sub("raspbian.raspberrypi.org", "legacy.raspbian.org", buff);
-    print buff "\n# " $0
+    if ($0 ~ /http:\/\/raspbian.raspberrypi.org/){
+      buff = $0;
+      sub("raspbian.raspberrypi.org", "legacy.raspbian.org", buff);
+      print buff "\n# "
+    }
+    print
   } else {
     print
   }
