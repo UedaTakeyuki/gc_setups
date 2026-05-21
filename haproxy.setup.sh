c1b774a5431292d8613adbdbe514b4f047066038
Dr. Takeyuki Ueda <ueda@latelierdueda.com>
2026-05-21 15:44:09 +0900

    Update haproxy.setup.sh

--- haproxy.setup.sh
+++ haproxy.setup.sh
@@ -3,3 +3,4 @@ sudo apt-get -y install haproxy
 # confirmed
 ## 2020-06-22 Linux scw-a 4.9.93-mainline-rev1 #1 SMP Tue Apr 10 09:42:40 UTC 2018 armv7l GNU/Linux
 ## 2026-05-20 Linux localhost 6.12.86+deb13-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.12.86-1 (2026-05-08) x86_64 GNU/Linux
+## 2026-05-21 Linux d 6.1.0-44-amd64 #1 SMP PREEMPT_DYNAMIC Debian 6.1.164-1 (2026-03-09) x86_64 GNU/Linux
